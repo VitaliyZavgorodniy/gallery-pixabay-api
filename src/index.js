0f5adb1df58d236883ef126efc35bac510c667f0
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-03-22 23:02:14 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-import _ from "lodash";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
+import "./css/normalize.css";
 import "./css/styles.css";
+
+import debounce from "lodash.debounce";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+import { fetchPhotos } from "./js/fetchPhotos";
+import { renderPhotos } from "./js/renderPhotos";
+
+const inputSearch = document.querySelector("[name='searchQuery']");
+const fetchData = {
+  value: null,
+  hits: [],
+  totalHits: 0,
+  page: 1,
+};
+
+const handleSearch = (e) => {
+  const { value } = e.target;
+  fetchData.page = 1;
+
+  fetchPhotos(value, 1)
+    .then((data) => {
+      if (data.hits.length) {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+        fetchData.value = value;
+        fetchData.totalHits = data.totalHits;
+        fetchData.hits = data.hits;
+        renderPhotos(fetchData.hits);
+      } else
+        Notify.failure(
+          "Sorry, there are no images matching your search query. Please try again."
+        );
+    })
+    .catch((e) => console.error(e));
+};
+
+window.onscroll = () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+    fetchData.hits.length < fetchData.totalHits
+  ) {
+    fetchData.page++;
+
+    fetchPhotos(fetchData.value, fetchData.page)
+      .then((data) => {
+        if (data.hits.length) {
+          fetchData.hits = fetchData.hits.concat(data.hits);
+
+          renderPhotos(fetchData.hits);
+        }
+      })
+      .catch((e) => console.error(e));
+  }
+};
+
+inputSearch.addEventListener("input", debounce(handleSearch, 500));
---
+++ src/js/renderPhotos.js
@@ -0,0 +1,50 @@
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryBox = document.querySelector(".gallery");
+const galleryLigthbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
+
+export const renderPhotos = (data) => {
+  const elementHTML = data
+    .map(
+      ({
+        previewURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+              <a class="photo-card" href="${largeImageURL}">
+                <img src="${previewURL}" alt="${tags}" title="${tags}" loading="lazy" />
+                <div class="info">
+                  <p class="info-item">
+                    <b>Likes</b>
+                    ${likes}
+                  </p>
+                  <p class="info-item">
+                    <b>Views</b>
+                    ${views}
+                  </p>
+                  <p class="info-item">
+                    <b>Comments</b>
+                    ${comments}
+                  </p>
+                  <p class="info-item">
+                    <b>Downloads</b>
+                    ${downloads}
+                  </p>
+                </div>
+              </a>
+              `
+    )
+    .join("");
+
+  galleryBox.innerHTML = elementHTML;
+
+  galleryLigthbox.refresh();
+};
