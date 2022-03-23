a1d39f5bceba757694ccb0576333eb8ad0cf7240
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-03-23 02:23:58 +0200

    removed comments

--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const API_KEY = "18651709-2ed9926c287754060d45cde83";
 const API_URL = "https://pixabay.com/api/";
 
-// const params = `per_page=40&image_type=photo&orientation=horizontal&safesearch=true`;
-
 export const fetchPhotos = (value, page = 1) =>
   axios
     .get(API_URL, {
